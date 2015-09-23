1a3684a39fbde2633d0ef8c954d52e6c9049b78f
Matteo Valoriani <m-atteo@hotmail.it>
2015-09-23 12:48:29 +0200

    added project template

---
+++ Samples/RealSense Project/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using RealSenseWrapper.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RealSense_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        private MainViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            vm = new MainViewModel();
+            vm.Intialize();
+
+
+            this.DataContext = vm;
+
+        }
+
+    }
+}
